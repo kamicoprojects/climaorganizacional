4cf3e1c64af0d2d6ee2cadf6f305570166bc0601
kamicoprojects <gitkamico@gmail.com>
2022-06-09 18:50:13 -0300

    update files

--- main.py
+++ main.py
@@ -2,43 +2,63 @@ from dash import Dash, html, dcc, Input, Output
 import plotly.express as px 
 import pandas as pd
 
-
-df = pd.read_excel('Pesquisa_Kami_Dados_Tratados.xlsx')
+# start server
 app = Dash(__name__)
+# read data_frame from xlsx file
+df = pd.read_excel('Pesquisa_Kami_Dados_Tratados.xlsx')
 
-fig = px.bar(df, x='Respostas', color='Setor', barmode='group')
+#fig = px.bar(df, x='Respostas', color='Setor', barmode='group')
 
-app.layout = html.Div(children=[
+app.layout = html.Div([
     html.H1('PESQUISA DE CLIMA ORGANIZACIONAL'),
     html.H2('Gráficos separados por departamentos'),      
     
-    html.P("Selecione um Departamento:"),    
-    dcc.Dropdown(df['Setor'].unique(), 
-                value='Todos Departamentos', id='setor-values', clearable=False ),
-    
-     
-    
+       
+    html.P("Selecione uma pergunta:"),    
+    html.Div([
+    dcc.Dropdown(df['Perguntas'], 
+                value='Perguntas', id='coluna-perguntas', clearable=False,
+                style={'width': '80%'}),
+            ]),
 
-    dcc.Graph(id='pesquisa-graph', figure=fig),   
+    dcc.Graph(id='pesquisa-graph',
+              style={'border': '0px solid black'}),   
    
 ])
 
 # Callback para o gráfico
 @app.callback(
     Output("pesquisa-graph", "figure"), 
-    Input("setor-values", "value"), 
+    Input("coluna-perguntas", "value"), 
     
-    )
-
-def generate_chart(value):
-    if value == 'Todos Departamentos':
-        fig = px.bar(df, x='Respostas', color='Setor', barmode='group')
+)
+def uptdate_setor(coluna_perguntas_nome):
+    if coluna_perguntas_nome == 'Perguntas':
+        #fig = px.bar(df, x='Respostas', color='Setor', barmode='group')
+        fig = px.bar(x=df[df['Respostas'] == coluna_perguntas_nome]['Respostas'],
+        hover_name=df[df['Perguntas'] == coluna_perguntas_nome]['Respostas'])
     else:
-        filter = df.loc[df['Setor'] == value, :]
+        filter = df.loc[df['Perguntas'] == coluna_perguntas_nome, :]
         fig = px.bar(filter, x='Respostas', color='Setor', barmode='group')
-        
+
     return fig
 
+""" @app.callback(
+    Output("pesquisa-graph", "figure"), 
+    Input("perguntas-setor", "value"), 
+    
+)
+def uptdate_perguntas(coluna_perguntas_nome):
+    if coluna_perguntas_nome == 'Perguntas':
+        #fig = px.bar(df, x='Respostas', color='Setor', barmode='group')
+        fig = px.bar(x=df[df['Respostas'] == coluna_perguntas_nome]['Respostas'],
+        hover_name=df[df['Setor'] == coluna_perguntas_nome]['Respostas'])
+    else:
+        filter = df.loc[df['Setor'] == coluna_perguntas_nome, :]
+        fig = px.bar(filter, x='Respostas', color='Setor', barmode='group')
+
+    return fig
+ """
 
 if __name__ == '__main__':
     app.run_server(debug=True)
